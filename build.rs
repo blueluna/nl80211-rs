aef066ada6f5e428de59325ff08b12656d61f8f3
Erik Svensson <erik.public@gmail.com>
2018-10-15 22:36:29 +0200

    Use OUT_DIR in build script

--- build.rs
+++ build.rs
@@ -5,10 +5,13 @@ use std::fs::File;
 use nl80211_buildtools::Specification;
 
 fn main() {
+    use std::env;
+    let out_dir = env::var("OUT_DIR").unwrap();
+
     let file = File::open("specifications/nl80211_commands.json").unwrap();
     match Specification::read(file) {
         Ok(spec) => {
-            spec.generate("src/commands.rs").unwrap()
+            spec.generate(&format!("{}/commands.rs", out_dir)).unwrap()
         }
         Err(error) => {
             panic!("Failed to load specification, {}", error);
@@ -18,7 +21,7 @@ fn main() {
     let file = File::open("specifications/nl80211_attributes.json").unwrap();
     match Specification::read(file) {
         Ok(spec) => {
-            spec.generate("src/attributes.rs").unwrap();
+            spec.generate(&format!("{}/attributes.rs", out_dir)).unwrap();
         }
         Err(error) => {
             panic!("Failed to load specification, {}", error);
@@ -28,7 +31,7 @@ fn main() {
     let file = File::open("specifications/information_element_id.json").unwrap();
     match Specification::read(file) {
         Ok(spec) => {
-            spec.generate("src/information_element_ids.rs").unwrap();
+            spec.generate(&format!("{}/information_element_ids.rs", out_dir)).unwrap();
         }
         Err(error) => {
             panic!("Failed to load specification, {}", error);
---
+++ src/information_element_ids.rs
@@ -0,0 +1,1 @@
+include!(concat!(env!("OUT_DIR"), "/information_element_ids.rs"));
